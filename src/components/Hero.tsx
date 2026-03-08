c98496ee5503cacced7db8af71813bf17f0074c1
Rex <Rex@Seths-Mac-mini.local>
2026-03-07 23:52:36 -0600

    V12.60: shift bg image up more (25%) to clear sign from text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -57,7 +57,7 @@ export default function Hero() {
         }
         .hero_bg-image {
           object-fit: cover;
-          object-position: center 35%;
+          object-position: center 25%;
         }
         .hero_overlay {
           position: absolute;
